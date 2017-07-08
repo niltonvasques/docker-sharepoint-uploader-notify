261f3be0e7763749fcce9fcd25b11326b51964e2
Nilton Vasques <nilton.vasques@openmailbox.org>
2017-07-08 05:46:31 -0300

    Fix send_mail #! format

--- send_mail.rb
+++ send_mail.rb
@@ -1,10 +1,9 @@
+#!/usr/bin/env ruby
 # Author: Nilton Vasques <nilton.vasques{at}openmailbox.org>
-# Description: This script connect upload a file to sharepoint and send a mail with this file link
+# Description: This script upload a file to sharepoint and send a mail with this file link
 #
 # SYNOPSIS
 # docker run -it --rm -e FROM='test@gmail.com' -e TO='test@gmail.com' -e SUBJECT='topic' -e SHAREPOINT_LOGIN='user@mail.com' -e SHAREPOINT_PASS='pass' -e MAIL_PASS='pass' -e SHAREPOINT_URL='sharepoint.com' -e SHAREPOINT_SITE='site' -e SHAREPOINT_FOLDER='folder' -e FILE_NAME='test.file' -v $PWD:/tmp/ --name mail-app mail-sharepoint-storage
-
-#!/usr/bin/env ruby
 
 require 'mail'
 require 'sharepoint-ruby'
